d37afc83128ee9b27ebec761ab0d83ffc9ad9891
Chidong Kim <jqueryutil@gmail.com>
2013-04-14 01:59:00 +0900

    isEmpty

---
+++ jquery.util.js
@@ -0,0 +1,21 @@
+/*!
+ * jQuery Util 1.0.0
+ * http://jqueryutil.com
+ *
+ * Copyright 2013 jQuery Util
+ * Released under the MIT license.
+ */
+
+(function($, undefined) {
+	$.isEmpty = function(s) {
+		if($(s).val() == "")
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		};
+	}
+)(jQuery);
